0da72426109b5125809ff2cc733a7ddaaec4f71e
Donguk-Kim-kr <loveyouduk@naver.com>
2026-08-28 12:29:34 +0900

    Update db_connection.py

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,20 +1,36 @@
 '''
 database/db_connection.py - PostgreSQL 연결 + 세션 의존성
 '''
+import streamlit as st
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# moviedb 데이터베이스가 먼저 생성되어 있어야 함. 계정/비밀번호는 각자 환경에 맞게 수정
-DATABASE_URL = 'postgresql+psycopg2://postgres:1234@localhost:5432/moviedb1'
+DATABASE_URL = st.secrets["DATABASE_URL"]
 
-engine = create_engine(DATABASE_URL, echo=True)
+@st.cache_resource
+def get_engine():
+    return create_engine(
+        DATABASE_URL,
+        pool_pre_ping=True,
+    )
 
-SessionFactory = sessionmaker(autocommit=False, autoflush=False, expire_on_commit=False, bind=engine)
+engine = get_engine()
 
+SessionFactory = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False,
+    bind=engine,
+)
 
 def get_session():
     session = SessionFactory()
     try:
         yield session
+
+    except Exception:
+        session.rollback()
+        raise
+
     finally:
         session.close()
